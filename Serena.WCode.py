29c8bd715fc97d242e3d15caf9f0f82922f0bcfb
SerenaW24 <51681153+SerenaW24@users.noreply.github.com>
2019-06-10 17:53:36 -0500

    Add files via upload

---
+++ Serena.WCode.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+""" 
+Created on Mon Jun 10 16:08:25 2019
+
+@author: admin
+"""
+#Documentation 
+#need perentaces when using words and quotes
+#number does not need quotes 
+#exponent=**
+
+# steps for function
+# 1)define by giving a name and perameters
+# 2)action
+# 3)test your function 
+
+# wrinting code to sum any two number 
+
+def sum(a,b): 
+    sum = a + b
+    
+sum(5,8)
+5/8 #Division
+print("Hello",5,"World")
+print("I love Python")
+print("I learned to code python today")
+print(5)
+5*2 # 5 multiplied by 2
+5/2 # 5 divide by 2
+5-2 # 5 subtracted by 2
+5**2 # 5 raised to the power of 2
+(8/9)*3 # 8 divide by 9 multiplied by 3 
