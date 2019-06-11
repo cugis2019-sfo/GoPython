2d5ab2ac86c49c2c5d2820bff277d6ce26b3b157
SerenaW24 <51681153+SerenaW24@users.noreply.github.com>
2019-06-11 14:35:37 -0500

    Add files via upload

--- Serena.WCode.py
+++ Serena.WCode.py
@@ -14,6 +14,12 @@ Created on Mon Jun 10 16:08:25 2019
 # 2)action
 # 3)test your function 
 
+# pi = 3.14 or 22/7
+# e = 2.7
+# power funtion, squareroot, factorial are not scaler 
+# scaler are numbers that stay the same 
+# factorial = 5!=5*4*3*2*1
+
 # wrinting code to sum any two number 
 
 def sum(a,b): 
@@ -60,6 +66,36 @@ def greeting(b):
     
 greeting("how are you doing?")
     
+# use "import math" to import file
+
+import math
+dir(math)
+
+math.pi
+# e = the sign e 
+math.e
+# when doing non scaler you will need to put in perentaces
+math.sqrt(4)
+# math factorial is the multiplecation of the number
+math.factorial(100)
+math.pow(3,10)
+math.remainder(4,6)
+math.remainder(10,6)
+
+def cubicroot(c):
+    cubicroot = c
+    
+    print(math.pow(c,(1/3)))
+    
+cubicroot(8)
+
+def cuberoot(d):
+    cuberoot = d**(1/3)
+    
+    print(cuberoot)
+    
+cuberoot(8)
+
 5/8 #Division
 print("Hello",5,"World")
 print("I love Python")
@@ -70,3 +106,5 @@ print(5)
 5-2 # 5 subtracted by 2
 5**2 # 5 raised to the power of 2
 (8/9)*3 # 8 divide by 9 multiplied by 3 
+
+
