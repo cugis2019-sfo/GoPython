99fb8cece2fdf89425f175dc015b182879329687
SerenaW24 <51681153+SerenaW24@users.noreply.github.com>
2019-06-12 12:56:02 -0500

    Add files via upload

---
+++ Day 3 Serena.WCode.py
@@ -0,0 +1,28 @@
+# Character = string 
+# Numeric = integer 
+
+# CadburyBox Math
+
+# CadburyBox = 5
+# CadburyBox = CadburyBox + 5
+
+# sum(CadburyBox + 5)
+
+CadburyBox1 = "Milk Chocolate"
+#  ||             ||
+# name        value=numeric or character
+# change name will change all rather than all need individual change 
+CadburyBox2 = "Dark Chocolate"
+CadburyBox3 = "White Chocolate"
+
+Cadburymilk = 5
+Cadburydark = 3
+Cadburywhite = 8
+
+# dict = the combining of the two variable 
+# {} use to define the pair data 
+Chocolate1 = {"Cadburymilk":5} # (milk,5)
+Chocolate2 = {"Cadburydark":8} # (dark,8)
+Chocolate3 = {"Cadburywhite":3} # (white,3)
+
+print(Chocolate1, Chocolate2, Chocolate3)
